58b46fee8a859d035e0da2e1c168feab982b16b5
pushka <pushka-k@mail.ru>
2014-05-13 10:30:29 +0400

    Added attributes for object fields.

---
+++ Bwl.Framework/src/Attributes/DisplayAttribute.vb
@@ -0,0 +1,8 @@
+﻿Public Class DisplayAttribute
+    Inherits Attribute
+    Public Property DisplayName As String
+
+    Public Sub New(displayName As String)
+        _DisplayName = displayName
+    End Sub
+End Class
---
+++ Bwl.Framework/src/Attributes/StateAttribute.vb
@@ -0,0 +1,6 @@
+﻿Public Class StateAttribute
+    Inherits DisplayAttribute
+    Public Sub New(displayName As String)
+        MyBase.New(displayName)
+    End Sub
+End Class
