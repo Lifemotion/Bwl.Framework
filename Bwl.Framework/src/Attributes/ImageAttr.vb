66b0896c8240886028718f0ab593bafd96e200f8
pushka <pushka@PAVELCHE>
2014-09-13 13:21:28 +0400

    Добавлен атрибут для пометки свойств с изображениями.

---
+++ Bwl.Framework/src/Attributes/ImageAttr.vb
@@ -0,0 +1,20 @@
+﻿''' <summary>
+''' Отображаемое поле или свойтсво объекта
+''' </summary>
+''' <remarks></remarks>
+Public Class ImageAttr
+	Inherits DisplayAttribute
+
+	Private ReadOnly _showIfNothing As Boolean
+
+	Public ReadOnly Property ShowIfNothing As Boolean
+		Get
+			Return _showIfNothing
+		End Get
+	End Property
+
+	Public Sub New(displayName As String, showIfNothing As Boolean, Optional visibleForAdminOnly As Boolean = False)
+		MyBase.New(displayName, visibleForAdminOnly)
+		_showIfNothing = showIfNothing
+	End Sub
+End Class
