31c102ffcf38d6a309be24dff2baac5d6e65bbbc
draf <artem.drobanov@gmail.com>
2016-06-20 09:16:32 +0300

    Small fix.

--- bwl.Network.ClientServer.Remoting/Common/BaseClient.vb
+++ bwl.Network.ClientServer.Remoting/Common/BaseClient.vb
@@ -2,7 +2,7 @@
     Protected _prefix As String
     Protected _client As IMessageClient
 
-    Public Sub New(netClient As NetClient, prefix As String)
+    Public Sub New(netClient As IMessageClient, prefix As String)
         _prefix = prefix
         _client = netClient
     End Sub
