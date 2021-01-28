40d249f4b6c8dc9ec016cf8d55456cf214d6c73d
draf <artem.drobanov@gmail.com>
2021-01-28 11:41:04 +0300

    Добавлен метод-расширение для SaveSettingsAuto для автоматического определения нужного варианта сохранения (обычные настройки/буферизированные).

---
+++ Bwl.Framework/src/Settings/Storages/SettingsStorageTools.vb
@@ -0,0 +1,30 @@
+﻿Imports System.Runtime.CompilerServices
+
+Public Module SettingsStorageTools
+    ''' <summary>
+    ''' Автоматический выбор нужного режима сохранения настроек.
+    ''' </summary>
+    ''' <param name="settings"></param>
+    <Extension()>
+    Public Sub SaveSettingsAuto(settings As SettingsStorageBase)
+        If settings.Parent IsNot Nothing Then
+            SaveSettingsAuto(settings.Parent)
+        Else
+            If TypeOf settings Is SettingsStorageBufferedRoot Then
+                DirectCast(settings, SettingsStorageBufferedRoot).WriteIniFile() 'Вызов SaveSettings(False) происходит внутри
+            Else
+                DirectCast(settings, SettingsStorage).SaveSettings(False) 'Обычное сохранение
+            End If
+        End If
+    End Sub
+
+    <Extension()>
+    Public Sub SaveSettingsAuto(settings As SettingsStorage)
+        SaveSettingsAuto(DirectCast(settings, SettingsStorageBase))
+    End Sub
+
+    <Extension()>
+    Public Sub SaveSettingsAuto(settings As SettingsStorageRoot)
+        SaveSettingsAuto(DirectCast(settings, SettingsStorageBase))
+    End Sub
+End Module
